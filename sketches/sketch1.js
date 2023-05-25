1f5f56676c73570aec472d04fdc990c58a311e38
Madisonn3 <126807576+Madisonn3@users.noreply.github.com>
2023-05-25 12:09:36 -0400

    Add files via upload

---
+++ sketches/sketch1.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill ("blue")
+  circle (200,200,300)
+  fill('purple')
+  triangle (190,168,222,229,140,227)
+  fill ('white')
+  circle (120,160,60)
+  circle (250,160,60)
+  fill ('black')
+  circle (120,160,30)
+  circle (250,160,30)
+  circle (200,280,80)
+}
